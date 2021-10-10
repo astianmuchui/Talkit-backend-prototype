390aeade2853e324b9a12e25a462a3b48fa9ce65
Sebastian <Sebastianmuchui79@gmail.com>
2021-10-10 18:59:01 +0300

    Update create_room.php

--- action/create_room.php
+++ action/create_room.php
@@ -1,6 +1,5 @@
 <?php
-    if(isset($_GET['id'])){
-        
+    if(isset($_GET['id'])){ 
         require_once '../classes/classes.php';
         $NewRoom = new chatroom();
         $NewRoom->BuildRoom();
